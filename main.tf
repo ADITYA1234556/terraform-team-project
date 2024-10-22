19bfb094aa58b0509d953c1fe661e672f689d406
aditya <aditya@gmail.com>
2024-10-22 17:53:19 +0100

    test

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@
 data "aws_vpc" "default" {
   default = true
 }
-
-
-
-
+1
+2
+3
+4
 # output "instance_public_ips" {
 #   value = [
 #   for instance in data.aws_autoscaling_group.asgips.instances : instance.public_ip
