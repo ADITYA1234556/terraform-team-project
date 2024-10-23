cc68432c1e2ac34e0bbee665d1179fcce7d4bee5
Ramshad <ramshadei@gmail.com>
2024-10-23 17:08:49 +0100

    3 commit

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,21 @@ output "subnet_id" {
   description = "The ID of the Subnet created by this Terraform configuration"
 }
 
+output "subnet_id" {
+  value       = aws_subnet.terraform_subnet.id
+  description = "The ID of the Subnet created by this Terraform configuration"
+}
+
+output "subnet_id" {
+  value       = aws_subnet.terraform_subnet.id
+  description = "The ID of the Subnet created by this Terraform configuration"
+}
+
+output "subnet_id" {
+  value       = aws_subnet.terraform_subnet.id
+  description = "The ID of the Subnet created by this Terraform configuration"
+}
+
 output "asg_name" {
   value       = aws_autoscaling_group.terraform_asg.name
   description = "The name of the Auto Scaling Group"
